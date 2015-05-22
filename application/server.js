1db5c3735406513763005b6642b8332e4e38ce46
Yanislav Shterev <yan6pz@gmail.com>
2015-05-22 23:24:50 +0300

    Reverse deleted paths

--- application/server.js
+++ application/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/html'));
 app.use(express.static(__dirname + '/javascript'));
 app.use(express.static(__dirname + '/libs'));
