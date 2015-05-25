240584c83e7bed1447938bb47c4392c9e7bc901e
mimipaskova <maria.paskova3@gmail.com>
2015-05-26 00:22:22 +0300

    Change photo with edit profile. Add 'Този профил съществува' page when name=='maria'

--- application/javascript/register.js
+++ application/javascript/register.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
 		window.location.href = "register.html";
 	});
 });
+
+
+
+$(document).ready(function () {
+	$("#register").on("click", function() {
+		if(document.getElementById("nameOrEmail").value == "maria") {
+			window.location.href = "user-exists.html";
+		}
+		else {
+			window.location.href = "profile.html";			
+		}
+	});
+});
--- application/server.js
+++ application/server.js
@@ -5,6 +5,7 @@ app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/html'));
 app.use(express.static(__dirname + '/javascript'));
 app.use(express.static(__dirname + '/libs'));
+app.use(express.static(__dirname + '/images'));
 app.listen(8080);
 
 console.log("Server started at 8080...");
