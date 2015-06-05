6080f353589084a88aac6d51030a6b029b785362
Nikolay Stoykov <nikolaisstoykov@gmail.com>
2015-06-05 17:48:32 +0300

    Statistics page - almost ready.

--- application/javascript/rating.js
+++ application/javascript/rating.js
@@ -1,3 +1,7 @@
 var gotoStatistics = function () {
 	loadInContainer("root-container", "statistics.html");
 }
+
+var gotoRating = function () {
+	loadInContainer("root-container", "rating.html");	
+}
